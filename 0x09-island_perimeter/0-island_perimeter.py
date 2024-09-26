8e50708fd7283ce10de9c6e1a699da7f59f903c0
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-09-26 22:35:56 +0000

    <create> island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Island perimeter computing module."""
+
+
+def island_perimeter(grid):
+    """Calculate island perimeter."""
+    if not grid:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == cols - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == rows - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+
+    return perimeter
